a9745b2ba9137838e1e748229525a95fbd2c100b
aslidogramaci <asli.dogramaci@std.yeditepe.edu.tr>
2025-11-02 20:52:31 +0300

    add the assignment

--- assignment1.py
+++ assignment1.py
@@ -3,8 +3,7 @@ def find_period(L0,L1):
     if L1>L0>0:
         for l in range(L0,L1+1,1):
             g = 9.81 #in m/s^2
-            T0 = 2 * np.pi * np.sqrt(L0/g)
-            T1 = 2 * np.pi * np.sqrt(L1/g)
+            T = 2 * np.pi * np.sqrt(L/g)
             print("Output is %f" %t)
             return (T0,T1)
 
